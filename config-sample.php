87e76623cdd4efb80d22c406485b4d7f65a5be5b
Michael D Adams <michael.d.adams@gmail.com>
2015-01-09 02:32:58 -0800

    I'm Awesome :)

---
+++ config-sample.php
@@ -0,0 +1,7 @@
+<?php
+
+defined( 'CHAT' ) or exit();
+
+$password = '';
+$api_token = '';
+$user_key = '';
--- init.php
+++ init.php
@@ -3,10 +3,11 @@
 defined( 'CHAT' ) or exit();
 
 require 'functions.php';
+require 'config.php';
 
 if ( ! isset( $_SERVER['PHP_AUTH_USER'] ) ) {
 	authenticate();
-} else if ( 'mdrchatme' !== $_SERVER['PHP_AUTH_PW'] ) {
+} else if ( $password !== $_SERVER['PHP_AUTH_PW'] ) {
 	authenticate();
 }
 
